1a270edfa43528a56d9faa356a22c67bc95bb254
Greg Dardis <dardis.greg@gmail.com>
2017-05-15 08:58:18 -0600

    score is calculated and added for one to six, can't pick same category twice

--- Yahtzee.java
+++ Yahtzee.java
@@ -69,24 +69,29 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 		display.displayDice(dice);
 	}
 	
+	/* Finds the score for categories ONES to SIXES, and returns it */
+	private int findScoreOnesToSixes(int category) {
+		int score = 0;
+		for (int i = 0; i < N_DICE; i++) {
+			if (dice[i] == category) {
+				score += dice[i];
+			}
+		}
+		return score;
+	}
+	
 	/* Takes an index for a category and checks to see if that category is met.
 	 * Returns a score to be added to the correct place in the scoreboard by a later method*/
 	private int findScoreForCategory(int category) {
 		int score = 0;
-		if (category == ONES) {
-			for (int i = 0; i < N_DICE; i++) {
-				if (dice[i] == 1) {
-					score += dice[i];
-				}
-			}
-			return score;
-		}
+		score = findScoreOnesToSixes(category);
+		
 		return score;
 //		if (YahtzeeMagicStub.checkCategory(dice, ONES)) {}
 	}
 	
 	/* Go through a turn based on who's turn it is */
-	private void takeTurn(int zeroIndexedPlayerTurn, int[][] scoreboard) {
+	private void takeTurn(int zeroIndexedPlayerTurn) {
 		/* Waits for the player to click the Roll Dice button to start the first dice roll
 		 * Returns when the Roll Dice button is pressed. */
 		display.waitForPlayerToClickRoll(zeroIndexedPlayerTurn + 1);
@@ -97,23 +102,40 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 		
 		/* Returns which category the player selected for their dice */
 		int category = display.waitForPlayerToSelectCategory();
+		
+		/* If the category has already been selected, prompt the player to choose
+		 * another category */
+		while (categories[category][zeroIndexedPlayerTurn] == 1) {
+			display.printMessage("That category has already been selected!");
+			category = display.waitForPlayerToSelectCategory();
+		}
+		
 		int score = findScoreForCategory(category);
+		
+		/* Update both the scoreboard, and the nearly identical categories
+		 * array. The categories array will have a 0 in a spot if the category
+		 * hasn't been chosen, and a 1 if it has been chosen before */
+		scoreboard[category][zeroIndexedPlayerTurn] = score;
+		categories[category][zeroIndexedPlayerTurn] = 1;
 		display.updateScorecard(category, zeroIndexedPlayerTurn + 1, score);
 	}
 	
 	
 	private void playGame() {
-		int[][] scoreboard = new int[N_CATEGORIES][nPlayers];
-		/* zeroIndexedPlayerTurn == -1 because nobody has played yet */
-//		zeroIndexedPlayerTurn = selectPlayerTurn(zeroIndexedPlayerTurn);
+		/* Scoreboard keeps track of the score, categories keeps track of if
+		 * a category has been chosen yet */
+		scoreboard = new int[N_CATEGORIES + 1][nPlayers];
+		categories = new int[N_CATEGORIES + 1][nPlayers];
+
 		for (int i = 0; i < (nPlayers * 13); i++) {
 			zeroIndexedPlayerTurn = selectPlayerTurn(zeroIndexedPlayerTurn);
-			takeTurn(zeroIndexedPlayerTurn, scoreboard);
+			takeTurn(zeroIndexedPlayerTurn);
 		}
-		
 	}
 		
 /* Private instance variables */
+	int[][] scoreboard;
+	int[][] categories;
 	int[] dice = new int[N_DICE]; 
 	private int zeroIndexedPlayerTurn = -1;
 	private int nPlayers;
